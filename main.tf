0b0fbffdd2343d34b691ce94cdc1a95209520295
CodingManoj <mvp2612@hotmail.com>
2023-02-19 20:04:18 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "mongodb" {
-  source               = "./vendor/modules/vpc"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  source               = "./vendor/modules/mongodb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
 }
 
