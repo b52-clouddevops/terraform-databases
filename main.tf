adf2667e38f731f8ee2cbb39340ac77d32eaddc6
CodingManoj <mvp2612@hotmail.com>
2023-02-21 20:12:06 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ module "mongodb" {
   ENV                  = var.ENV
 }
 
+module "redis" {
+  source               = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                  = var.ENV
+}
