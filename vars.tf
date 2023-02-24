5d2803b254c0a685fe6bd63924683505e8e46531
CodingManoj <mvp2612@hotmail.com>
2023-02-23 20:36:29 -0500

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -5,3 +5,8 @@ variable "RDS_MYSQL_PORT" {}
 variable "RDS_MYSQL_STORAGE" {}
 variable "RDS_MYSQL_ENGINE_VERSION" {}
 variable "RDS_INSTANCE_TYPE" {}
+
+# DocDB 
+varibale "DOCDB_PORT" {}
+variable "DOCDB_INSTACE_CLASS" {}
+variable "DOCDB_INSTACE_COUNT" {}
