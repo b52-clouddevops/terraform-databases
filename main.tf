dabc335f7a0a8e7a4d639dba6a60c710c33f2c35
CodingManoj <mvp2612@hotmail.com>
2023-02-23 20:30:15 -0500

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -11,8 +11,12 @@ module "redis" {
 module "mysql" {
   source               = "./vendor/modules/mysql"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                  = var.ENV
-  RDS_MYSQL_PORT       = var.RDS_MYSQL_PORT 
+  RDS_MYSQL_PORT       = var.RDS_MYSQL_PORT
+  RDS_MYSQL_STORAGE    = var.RDS_MYSQL_STORAGE
+  RDS_MYSQL_ENGINE_VERSION = var.RDS_MYSQL_ENGINE_VERSION
   
+
+
 }
 
 output "redis" {
