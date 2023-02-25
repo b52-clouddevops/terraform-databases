4c6410c070e0fe441f2cee08af0a2376e4127991
CodingManoj <mvp2612@hotmail.com>
2023-02-24 19:20:01 -0500

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -16,10 +16,3 @@ variable "REDIS_PORT" {}
 variable "REDIS_INSTANCE_NODETYPE" {}
 variable "REDIS_INSTANCE_COUNT" {}
 variable "REDIS_ENGINE_VERSION"
-
-
- node_type            = "cache.t3.micro"
-
- 6.x
-
- 6379
