9fcb9a0f23eb30d8834317703ce546f829391bd5
CodingManoj <mvp2612@hotmail.com>
2023-03-05 20:05:23 -0500

    Auto Push Commit

--- vars.tf
+++ vars.tf
@@ -16,3 +16,9 @@ variable "REDIS_PORT" {}
 variable "REDIS_INSTANCE_NODETYPE" {}
 variable "REDIS_INSTANCE_COUNT" {}
 variable "REDIS_ENGINE_VERSION" {}
+
+# RabbitMQ
+
+variable "RABBITMQ_PORT{
+  
+}
